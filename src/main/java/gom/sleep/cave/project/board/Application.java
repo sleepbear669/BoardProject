643690ac3e22f85d7afe8deeee148afd9227988f
sleepbear <sleepbear669@gmail.com>
2016-06-01 16:05:24 +0900

    project init

---
+++ src/main/java/gom/sleep/cave/project/board/Application.java
@@ -0,0 +1,15 @@
+package gom.sleep.cave.project.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sleepbear on 2016. 5. 23..
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
